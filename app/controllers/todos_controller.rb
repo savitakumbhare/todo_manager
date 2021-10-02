14e2a64817af7c82f4885eb9ebf09ac0518662bc
savitakumbhare <savita.kumbhare21@gmail.com>
2021-10-03 02:43:05 +0530

    create a new todo, and mark an existing todo as completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
@@ -19,5 +21,14 @@ class TodosController < ApplicationController
     )
     response_text = "Hey, your new todo is created with id #{new_todo.id}"
     render plain: response_text
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: " Updated todo completed status to #{completed}"
   end
 end
