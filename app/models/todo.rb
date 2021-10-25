b20f6cce5cdf4cb55932a50488fb51b14a33434e
savitakumbhare <savita.kumbhare21@gmail.com>
2021-10-25 17:00:21 +0530

    Updated database migrations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,31 @@
+require "active_record"
+require "date"
+
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date =?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date >?", Date.today)
+  end
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def is_due_today?
+    due_date == Date.today
+  end
+
+  def is_completed?
+    Todo.where(completed == true)
+  end
+
+  def self.mark_as_complete(todo_id)
+    Todo.update(todo_id, :completed => true)
   end
 end
